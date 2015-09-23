3b367acbdcc2c8e6a7c51b82961a69a7f9fc5bb7
Adrien Descamps <adrien.descamps@gmail.com>
2015-09-23 22:51:24 +0200

    add class for motor control

---
+++ Ardyno/DynamixelMotor.cpp
@@ -0,0 +1,35 @@
+#include "DynamixelMotor.h"
+
+
+DynamixelMotor::DynamixelMotor(DynamixelInterface &aInterface, DynamixelID aId):
+	DynamixelDevice(aInterface, aId)
+{}
+
+
+void DynamixelMotor::wheelMode()
+{
+	jointMode(0,0);
+}
+
+void DynamixelMotor::jointMode(uint16_t aCWLimit, uint16_t aCCWLimit)
+{
+	uint32_t data= (aCWLimit | (uint32_t(aCCWLimit)<<16));
+	write(DYN_ADDRESS_CW_LIMIT, data);
+}
+
+void DynamixelMotor::enableTorque(bool aTorque)
+{
+	write(DYN_ADDRESS_ENABLE_TORQUE, uint8_t(aTorque?1:0));
+}
+
+void DynamixelMotor::speed(int16_t aSpeed)
+{
+	if(aSpeed<0)
+		aSpeed=-aSpeed || 1024;
+	write(DYN_ADDRESS_GOAL_SPEED, aSpeed);
+}
+
+void DynamixelMotor::position(uint16_t aPosition)
+{
+	write(DYN_ADDRESS_GOAL_POSITION, aPosition);
+}
---
+++ Ardyno/DynamixelMotor.h
@@ -0,0 +1,45 @@
+/**
+ * \file DynamixelMotor.h
+ * \brief Define classe for dynamixel motor
+*/
+
+#ifndef DYNAMIXEL_MOTOR_H
+#define DYNAMIXEL_MOTOR_H
+
+#include "Dynamixel.h"
+
+/**
+ * \brief Dynamixel motor control table addresses (only addresses used by all motor models)
+*/
+enum DynMotorAddress
+{
+	/** \brief Clockwise angle limit, uint16_t , writable */
+	DYN_ADDRESS_CW_LIMIT		=0x06,
+	/** \brief Counter clockwise angle limit, uint16_t , writable */
+	DYN_ADDRESS_CCW_LIMIT		=0x08,
+	/** \brief Maximum torque, uint16_t , writable */
+	DYN_ADDRESS_MAX_TORQUE		=0x0E,
+	
+	/** \brief Enable torque, uint8_t , writable */
+	DYN_ADDRESS_ENABLE_TORQUE		=0x18,
+	/** \brief Goal position, uint16_t , writable */
+	DYN_ADDRESS_GOAL_POSITION		=0x1E,
+	/** \brief Goal speed, uint16_t , writable */
+	DYN_ADDRESS_GOAL_SPEED		=0x20,
+};
+
+class DynamixelMotor:public DynamixelDevice
+{
+	public:
+	
+	DynamixelMotor(DynamixelInterface &aInterface, DynamixelID aId);
+	
+	void wheelMode();
+	void jointMode(uint16_t aCWLimit=0, uint16_t aCCWLimit=0x3FF);
+	
+	void enableTorque(bool aTorque=true);
+	void speed(int16_t aSpeed);
+	void position(uint16_t aPosition);
+};
+
+#endif
